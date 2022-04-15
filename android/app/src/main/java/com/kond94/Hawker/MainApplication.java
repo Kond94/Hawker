008ba1b668fe4dfc1cfdf3fcd56665bae596afdc
Kondwerani Kamsesa <kamsesakond@hotmail.com>
2022-04-15 20:27:30 +0200

    Remove unused assets and packages

--- android/app/src/main/java/com/kond94/Hawker/MainApplication.java
+++ android/app/src/main/java/com/kond94/Hawker/MainApplication.java
@@ -20,8 +20,6 @@ import com.facebook.react.bridge.JSIModulePackage;
 
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
-import com.facebook.react.bridge.JSIModulePackage; // <- add
-  import com.swmansion.reanimated.ReanimatedJSIModulePackage; // <- add
 
 public class MainApplication extends Application implements ReactApplication {
   private final ReactNativeHost mReactNativeHost = new ReactNativeHostWrapper(
@@ -45,11 +43,6 @@ public class MainApplication extends Application implements ReactApplication {
     protected String getJSMainModuleName() {
       return "index";
     }
-
-      @Override
-      protected JSIModulePackage getJSIModulePackage() {
-        return new ReanimatedJSIModulePackage(); // <- add
-      }
   });
 
   @Override
